c22f7e42e7cc6c03a3d7f5eff4ad9cc8bf82a3b2
EmanueleAnnoscia <annosciaemanuele@gmail.com>
2025-06-12 14:50:17 +0200

    fix bootstrap

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,22 +1,23 @@
 import { Link } from "react-router-dom";
 
 const Home = () => {
-  return (
-    <>
-      <main>
-        <h1>pagina HOME</h1>
-        <Link className="btn btn-primary" to="/AboutUs">
-          Chi Siamo
-        </Link>
-        <Link className="btn btn-warning" to="/CreatePost">
-          Crea un Nuovo Post
-        </Link>
-        <Link className="btn btn-warning" to="/PostsList">
-          Elenco dei Post
-        </Link>
-      </main>
-    </>
-  );
+    return (
+        <main className="container-fluid d-flex flex-column justify-content-center align-items-center min-vh-100">
+            <h1 className="text-center mb-5">HOMEPAGE</h1>
+
+            <div className="d-flex flex-column gap-4 w-100" style={{ maxWidth: "400px" }}>
+                <Link className="btn btn-primary btn-lg w-100" to="/AboutUs">
+                    Chi Siamo
+                </Link>
+                <Link className="btn btn-success btn-lg w-100" to="/CreatePost">
+                    Crea un Nuovo Post
+                </Link>
+                <Link className="btn btn-danger btn-lg w-100" to="/PostsList">
+                    Elenco dei Post
+                </Link>
+            </div>
+        </main>
+    );
 };
 
 export default Home;
