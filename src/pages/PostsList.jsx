eff4fd5fdef6d101038ff937da9c35a25d2272e2
EmanueleAnnoscia <annosciaemanuele@gmail.com>
2025-06-13 09:55:20 +0200

    limitato il numero degli elementi dei post visualizzati

--- src/pages/PostsList.jsx
+++ src/pages/PostsList.jsx
@@ -5,7 +5,7 @@ const PostsList = () => {
   const [posts, setPosts] = useState([]);
   useEffect(() => {
     axios
-      .get("https://67c5b4f3351c081993fb1ab6.mockapi.io/api/posts")
+      .get("https://67c5b4f3351c081993fb1ab6.mockapi.io/api/posts?page=1&limit=50")
       .then((resp) => {
         setPosts(resp.data);
       });
